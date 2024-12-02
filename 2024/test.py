7d31d335a3b34f8ad3dac0f2523d0c44546216ba
Petr Olmer <petr.olmer@gmail.com>
2024-12-02 21:18:27 +0100

    deleted test

--- 2024/test.py
+++
@@ -1,3 +0,0 @@
-l = [list(map(int, line.split())) for line in open('01input.txt').read().splitlines()][0]
-r = [list(map(int, line.split())) for line in open('01input.txt').read().splitlines()][1]
-print(sum(x * r.count(x) for x in l))
